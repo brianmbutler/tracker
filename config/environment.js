fbb146d09909fb3b873f7685a398013d5a31c542
Brian Butler <brian_m_butler@icloud.com>
2015-06-19 13:14:31 -0400

    add packages

--- config/environment.js
+++ config/environment.js
@@ -1,13 +1,31 @@
-/* jshint node: true */
+/* jshint node: false */
 
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'tracker',
+    podModulePrefix: 'tracker/pods',
+   // firebase: 'https://blinding-torch-7719.firebaseio.com/',
     environment: environment,
+    contentSecurityPolicy: {
+       'default-src': "'none'",
+       'font-src': "'self' https://fonts.gstatic.com",
+       'connect-src': "'self' http://localhost:5984",
+       'frame-src': "'self' http://*.firebaseio.com",
+       'script-src': "'self' 'unsafe-eval' 'unsafe-inline' http://*.firebaseio.com",
+       'img-src': "'self'",
+       'style-src': "'self 'unsafe-inline' https://fonts.googleapis.com"
+          },
+
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
+        'ember-htmlbars-attribute-syntax': true,
+        'ember-htmlbars-inline-if-helper': true,
+        'ember-htmlbars-component-generation': true,
+        'ember-routing-named-substates': true,
+        'ember-views-component-block-info': true,
+        'ember-routing-htmlbars-improved-actions': true
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
       }
@@ -21,10 +39,10 @@ module.exports = function(environment) {
 
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
+    ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
 
   if (environment === 'test') {
@@ -39,9 +57,16 @@ module.exports = function(environment) {
     ENV.APP.rootElement = '#ember-testing';
   }
 
+ENV.remote_couch = 'http://localhost:5984/employees';
+ENV.local_couch = 'employees';
+
   if (environment === 'production') {
+    ENV.baseURL = '/safetydoxcouch/';
+    ENV.remote_couch = 'http://safetydox.iriscouch.com/employee';
 
-  }
-
+ENV.contentSecurityPolicy = {
+  'connect-src': "'self' " + ENV.remove_couch.substring(0, ENV.remote_couch.indexOf('/', 9))
+};
+}
   return ENV;
 };
